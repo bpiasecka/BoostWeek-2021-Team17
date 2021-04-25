e3b59967a509b8d54a4ce3213ddcc515fb2d1b7c
patryczektak <patryk.jasinski@tensquaregames.com>
2021-04-25 13:15:25 +0200

    min shake

--- Assets/Patryk/cameraShake.cs
+++ Assets/Patryk/cameraShake.cs
@@ -13,8 +13,8 @@ public class cameraShake : MonoBehaviour
 
         while (elapsed < duration)
         {
-            float x = Random.Range(-0.5f, 0.5f) * magnitude;
-            float y = Random.Range(-0.5f,0.5f) * magnitude;
+            float x = Random.Range(-0.1f, 0.1f) * magnitude;
+            float y = Random.Range(-0.1f,0.1f) * magnitude;
 
             transform.localPosition = new Vector3(x, y, originPos.z);
 
